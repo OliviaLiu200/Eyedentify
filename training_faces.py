90d5f484515caaf20b46b54083b394c407dcaac3
Olivia <57505942+OliviaLiu200@users.noreply.github.com>
2022-01-09 17:02:01 -0500

    final product

--- training_faces.py
+++ training_faces.py
@@ -4,56 +4,55 @@ import numpy as np
 import pickle
 from PIL import Image
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-image_dir = os.path.join(BASE_DIR, "photos")
-
-recognizer = cv2.face.LBPHFaceRecognizer_create()  # declaring LBPH recognizer
-
-x_train = []
-y_labels = []
-cur_id = 0
-label_ids = {}
-
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()  # declaring LBPH recognizer
 face_cascade = cv2.CascadeClassifier(
     'cascades/data/haarcascade_frontalface_alt2.xml')
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+IMAGE_DIR = os.path.join(BASE_DIR, "photos")
+
+x_training = []
+y_label = []
+id_cur = 0
+labels = {}
+
 # os.walk for image finding
-for root, dirs, files in os.walk(image_dir):
+for root, dirs, files in os.walk(IMAGE_DIR):
     for file in files:
-        if file.endswith("png") or file.endswith("jpg") or file.endswith("PNG") or file.endswith("JPG"):
+        if file.endswith("png") or file.endswith("PNG") or file.endswith("jpg") or file.endswith("JPG"):
             path = os.path.join(root, file)
             label = os.path.basename(root).replace(
                 " ", "-").lower()  # labels from directory
             # print(path)
-            if label in label_ids:
+            if label in labels:
                 pass
             else:
-                label_ids[label] = cur_id
-                cur_id += 1
-
-            id_ = label_ids[label]
-            # print(label_ids)
+                labels[label] = id_cur
+                id_cur += 1
+            id = labels[label]
+            # print(labels)
 
             # train images to numpy arrays
-            pil_image = Image.open(path).convert("L")  # grayscale
-            image_array = np.array(pil_image, "uint8")
-            # print(image_array)
+            image = Image.open(path).convert("L").resize(
+                (600, 600), Image.ANTIALIAS)  # grayscale and resized for training
+
+            photos_array = np.array(image, "uint8")  # print(image_array)
 
             # find region of interests in training photos
             faces = face_cascade.detectMultiScale(
-                image_array, scaleFactor=1.5, minNeighbors=5)
+                photos_array, scaleFactor=1.5, minNeighbors=5)
 
             for (x, y, w, h) in faces:
-                roi = image_array[y:y+h, x:x+h]
-                x_train.append(roi)
-                y_labels.append(id_)
+                roi = photos_array[y:y+h, x:x+h]
+                x_training.append(roi)
+                y_label.append(id)
 
-# print(y_labels)
-# print(x_train)
+# print(x_training)
+# print(y_label)
 
 # save label ids
 with open("labels.pickle", 'wb') as f:
-    pickle.dump(label_ids, f)
+    pickle.dump(labels, f)
 
-recognizer.train(x_train, np.array(y_labels))
-recognizer.save("trainner.yml")
+face_recognizer.train(x_training, np.array(y_label))
+face_recognizer.save("trainner.yml")
